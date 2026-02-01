06c2d55d3712ca765c3bca06368ba7a7733b83e5
Zsolt Harmath <harmathzs@gmail.com>
2026-02-01 16:51:58 +0100

    Test GET /messages

--- react-app/src/backend/backend.test.mjs
+++ react-app/src/backend/backend.test.mjs
@@ -1,7 +1,22 @@
 import { app, port, connection } from "./server.mjs";
+import request from 'supertest'
 
-describe('Test Suite for server connections', ()=>{
-    test('port', ()=>{
+describe('Server connections', ()=>{
+    test('backend port', ()=>{
         expect(port).toBeGreaterThanOrEqual(0+1)
+    })
+    test('MySQL connection', ()=>{
+        expect(connection).not.toBeNull()
+    })
+    test('app', ()=>{
+        expect(app).not.toBeNull()
+    })    
+})
+
+describe('Endpoints', ()=>{
+    test('GET /messages', async ()=>{
+        // normal test
+        const res = await request(app).get('/messages')
+        expect(res).not.toBeNull()
     })
 })
