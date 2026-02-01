a715fd82f4d8ba80a9a4d129eb2edd3105dd6b19
Zsolt Harmath <harmathzs@gmail.com>
2026-02-01 16:10:12 +0100

    Prepare backend

--- react-app/src/backend/index.mjs
+++ react-app/src/backend/index.mjs
@@ -1,14 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+import mysql from 'mysql2'
 
-const app = express()
+export const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 
 
-const port = process?.env?.PORT || 3333
+export const port = process?.env?.PORT || 3333
 app.listen(port, (error)=>{
     if (error) console.warn(error)
     else console.log('Backend runs at port ', port)
