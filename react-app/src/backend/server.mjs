d2b6562596836a362a7dc0c417e8ec1416337ffb
Zsolt Harmath <harmathzs@gmail.com>
2026-02-01 17:32:21 +0100

    Add TODOs to backend

--- react-app/src/backend/server.mjs
+++ react-app/src/backend/server.mjs
@@ -20,7 +20,7 @@ export const connection = await mysql.createConnection({
 app.get('/messages', async (req, res)=>{
     try {
         const sql = 'SELECT id, thread_id, role, message_content FROM chat_completions ORDER BY id ASC LIMIT 100'
-        const [results, fields] = await connection.query(sql);
+        let [results, fields] = [[], []] // TODO
         console.log('results', results)
         return res.status(200).json({results, fields})
     } catch (error) {
@@ -32,9 +32,9 @@ app.get('/messages', async (req, res)=>{
 app.post('/messages', async (req, res)=>{
     try {
         const {role, message_content} = req.body
-        const values = [role, message_content]
-        const sql = 'INSERT INTO chat_completions (`role`, `message_content`) VALUES (?, ?)'
-        const [result, fields] = await connection.execute(sql, values)
+        const values = 'TODO'
+        const sql = 'INSERT INTO chat_completions TODO'
+        let [result, fields] = [[], []] // TODO
         console.log('result', result)
         return res.status(201).json({result, fields})
     } catch (error) {
